b5f24c885a84a7ff15a84394ffcd78bd014a1406
anicarr <chiadikobiani@gmail.com>
2023-08-19 01:49:59 +0100

    project Completed

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Hero from "./Hero";
+import Submenu from "./Submenu";
+
+const App = () => {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Sidebar />
+      <Submenu />
+    </main>
+  );
+};
+export default App;
